00c3ec6106f9878a92db9a0c87aa3a8c61d29aeb
Sahilmahalle <sahilvmahalle@gmail.com>
2024-03-13 21:34:02 +0530

    subscription model added

---
+++ src/models/subscriptions.models.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema, mongo } from "mongoose";
+
+const subsciptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subscription = mongoose.model("Subscription", subsciptionSchema);
